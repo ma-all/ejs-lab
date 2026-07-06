891c72b5c00d11e48d1b602bce1d4df32c4d8e67
ma-all <marwaalbaloushi21@gmail.com>
2026-07-06 20:10:21 +0300

    completed ex 1 only

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+
+const RESTAURANT = {
+  name: 'The Green Byte Bistro',
+  isOpen: true,
+  address: '742 Evergreen Rd, Mapleview, OS 45502',
+  phone: '555-321-9876',
+  menu: [
+    { 
+      id: 1,
+      name: 'Quantum Quinoa Mushroom Burger',
+      price: 13.00,
+      rating: 4,
+      category: 'mains',
+      details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+    },
+    { 
+      id: 2,
+      name: 'Binary Berry Cheesecake',
+      price: 10.11,
+      rating: 3,
+      category: 'desserts',
+      details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+    },
+    { 
+      id: 3,
+      name: 'Recursive Rigatoni',
+      price: 17.00,
+      rating: 5,
+      category: 'mains',
+      details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
+    },
+    { 
+      id: 4,
+      name: 'Pumpkin Pi Squared',
+      price: 3.14,
+      rating: 5,
+      category: 'desserts',
+      details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+    },
+    { 
+      id: 5,
+      name: 'Fibonacci String Bean Fries',
+      price: 11.23,
+      rating: 5,
+      category: 'sides',
+      details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
+    }
+  ]
+}
+
+//ex 1 - question 1
+app.get('/', function(req, res){
+  res.render('home.ejs', {
+    resturant: RESTAURANT
+  })
+});
+
+//ex 2 - question 1
+app.get('/menu', function(req, res){
+  res.render('menu.ejs', {
+    resturant: RESTAURANT.menu
+  })
+})
+
+//ex 3 - question 1
+app.get('/menu/:category', function(req, res){
+  const category = req.params.category
+  const menuitems = RESTAURANT.menu.filter(function(item){
+    return item.category === category
+  })
+  res.render('menu.ejs', {
+    menuitems, 
+    category,
+  })
+})
+
+app.listen(3000);
